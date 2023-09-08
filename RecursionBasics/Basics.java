041977fcbe353dde5a6bf04b1395d7c26ff7658e
ATRIKA SHOW <readeratrika08@gmail.com>
2023-09-08 12:37:48 +0530

    Sum f N natural number

--- RecursionBasics/Basics.java
+++ RecursionBasics/Basics.java
@@ -9,9 +9,22 @@ public class Basics{
         int ans = n * factorial(n-1);
         return ans;
     }
+
+
+    // calculate sum of n natural number using recursion in O(n) time & space complexity
+    public static int sumOfNaturalNum(int n){
+        // base case
+        if (n == 1) {
+            return 1;
+        }
+
+        // work
+        int ans = n + sumOfNaturalNum(n-1);
+        return ans;
+    }
     public static void main(String[] args) {
 
-        int ans = factorial(5);
+        int ans = sumOfNaturalNum(10);
         System.out.println(ans);
     }
 }
