0bd1e104d4f5fb6d51876d8ae3a4649c4a46b21a
ATRIKA SHOW <readeratrika08@gmail.com>
2023-09-25 11:59:30 +0530

    Search in Rotated & sorted Array using Iterations

--- Divide&Conquer/SearchInRotatedSortedArr.java
+++ Divide&Conquer/SearchInRotatedSortedArr.java
@@ -1,6 +1,6 @@
 public class SearchInRotatedSortedArr {
     // Time complexity = > O(logn)
-    
+
     public static int findTarget(int arr[], int si, int ei, int target) {
         // base case
         if (si > ei) {
@@ -9,35 +9,74 @@ public class SearchInRotatedSortedArr {
         // mid
         int mid = (si + ei) / 2;
         if (arr[mid] == target) {
-            return mid;            // return index of target element
+            return mid; // return index of target element
         }
         // case 1 : mid on line 1
         if (arr[si] <= arr[mid]) {
             // case a : targeted element in left side of mid
             if (arr[si] <= target && target <= arr[mid]) {
-                return findTarget(arr, si, mid-1, target); // search on left
-            } 
+                return findTarget(arr, si, mid - 1, target); // search on left
+            }
             // case b : targeted element in right side of mid
             else {
                 return findTarget(arr, mid + 1, ei, target);
             }
-        } 
+        }
         // case 2 : mid on line 2
         else {
             // case c: targeted element in right side of mid
             if (arr[mid] <= target && target <= arr[ei]) {
                 return findTarget(arr, mid + 1, ei, target);
-            } 
+            }
             // case d : targeted element in left side of mid
             else {
-                return findTarget(arr, si, mid-1, target);
+                return findTarget(arr, si, mid - 1, target);
             }
         }
     }
 
+    public static int search(int arr[], int tar, int si, int ei) {
+        int i = 0;
+        int idx = -1;
+        while (i < arr.length) {
+            if (si>ei) {
+                return -1;
+            }
+            int mid = si + (ei - si) / 2;
+
+            if (arr[mid] == tar) {
+                idx = mid;
+                break;
+            } else {
+                // case 1 : mid on line 1
+                if (arr[si] <= arr[mid]) {
+                    // case a : target element in left side
+                    if (arr[si] <= tar && tar <= arr[mid]) {
+                        ei = mid-1; // update ending index
+                    }
+                    else{
+                        si = mid+1; // update starting index
+                    }
+                }
+                // case 1 : mid on line 2
+                else {
+                    // case c: target element is in right side of mid
+                    if (arr[mid] <= tar && tar <= arr[ei]) {
+                        si = mid+1;
+                    }else{
+                        ei = mid-1;
+                    }
+                }
+            }
+
+        }
+        return idx;
+    }
+
     public static void main(String[] args) {
-        int arr[] = { 4, 5, 6, 7, 0, 1, 2 ,5};
-        int target = 1;
-        System.out.println(findTarget(arr, 0, arr.length - 1, target));
+        int arr[] = { 4, 5, 6, 7, 0, 1, 2, 5 };
+        int target = 10;
+        // System.out.println(findTarget(arr, 0, arr.length - 1, target));
+        System.out.println(search(arr, target, 0, arr.length-1));
     }
 }
